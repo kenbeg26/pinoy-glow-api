ca512e2566269b37094ffae26efb54db496cce76
kenbegornia2 <kenbegornia2@gmail.com>
2025-10-15 21:17:09 +0800

    setup pinow glow api

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-// index.js
+// Dependencies and Modules
 const express = require("express");
+const mongoose = require 
 const cors = require("cors");
 
+// Routes
+const userRoutes = require("./routes/user");
+
+// Environment Setup
+require('dotenv').config();
+
+// Server Setup
 const app = express();
+const port = 4002;
 
-// Middleware
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'http://localhost:3000'
+  ],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
-// Example route
-app.get("/", (req, res) => {
-  res.send("Hello, Express.js is running!");
-});
+mongoose.connect(process.env.MONGODB_STRING);
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
-});
+mongoose.connection.once('open', () => console.log("Now connected to MongoDB Atlas."));
+
+// Backend Routes
+app.use("user", userRoutes);
+
+if(require.main === module){
+  app.listen( process.env.PORT || 3000, () => {
+    console.log(`API is now online on port ${ process.env.PORT || 3000 }`)
+  });
+}
+
+module.exports = { app, mongoose };
